e55a1b89f5fca96f6fcd906f621b36195ee01bc2
Nehmie25 <boyernehemie@gmail.com>
2022-12-14 17:07:25 +0000

    first commit

--- src/pages/logistic/dispatch/Dispatch.js
+++ src/pages/logistic/dispatch/Dispatch.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { makeStyles, Paper } from '@material-ui/core';
+import { Grid, makeStyles, Paper, Toolbar } from '@material-ui/core';
 import { TimelineSeparator } from '@material-ui/lab';
 import React from 'react';
 import Controls from '../../../components/controls/Controls';
@@ -9,31 +9,13 @@ import DispatchForm from './DispatchForm';
 
 const useStyles = makeStyles(theme => ({
     pageContent: {
-        margin: theme.spacing(4),
-        padding: theme.spacing(3),
-        with: '50%'
+        margin: theme.spacing(2),
+        padding: theme.spacing(1),
         
     },
-    searchInput: {
-        width: '50%',
-        right: '1rem'
-    },
-    newButton: {
-        position: 'absolute',
-        right: '10px'
-    },
-    newButton1: {
-        position: 'absolute',
-        right: '9rem'
-    },
     head: {
-        position: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        margin: 5,
-        padding: 1,
         color: 'gray',
-        alignItem: 'center'
+
     }
 }))
 
@@ -46,7 +28,40 @@ function Dispatch(props) {
                 <DispatchForm />
             </Paper>
             <Paper className={classes.pageContent}>
-                <div className={classes.head}>
+                <Toolbar>
+                    <p style={{color: 'gray', paddingRight: '2px'}}>Preview Dispatch</p>
+                </Toolbar>
+                <Grid className={classes.head} container>
+                    <Grid item xs={4}>
+                        <div>
+                            <p>Batch number : {}</p>
+                            <p>Reference : {}</p>
+                            <p>Waybill number : {}</p>
+                        </div>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <div>
+                            <p>Sender First name : {}</p>
+                            <p>Sender Last name : {}</p>
+                            <p>Sender Phone : {}</p>
+                            <p>Sender Email : {}</p>
+                        </div>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <div>
+                            <p>Receiver First name : {}</p>
+                            <p>Receiver Last name : {}</p>
+                            <p>Receiver Phone : {}</p>
+                            <p>Receiver Email : {}</p>
+                        </div>
+                        <br />
+                        <Controls.Button 
+                            type='submit'
+                            text='DISPATCH'
+                        />
+                    </Grid>
+                </Grid>
+                {/* <div className={classes.head}>
                     <div>
                         <p>Batch number : {}</p>
                         <p>Reference : {}</p>
@@ -66,11 +81,7 @@ function Dispatch(props) {
                         <p>Receiver Phone : {}</p>
                         <p>Receiver Email : {}</p>
                     </div>
-                </div>
-                <Controls.Button 
-                    type='submit'
-                    text='DISPATCH'
-                />
+                </div> */}
             </Paper>
         </>
     );
