f787537915f7e3a0dc317d00d20a084becbb83c1
Nehmie25 <boyernehemie@gmail.com>
2022-12-19 08:00:59 +0000

    first commit

--- src/App/App.js
+++ src/App/App.js
@@ -60,9 +60,9 @@ function App() {
         <ThemeProvider theme={theme}>
           <div className={classes.appMain}>
             <Switch>
-              <Route path="/" exact component={ Login } />
-              <Route path="/dashboard" exact component={ dashboard } />
-              <Route path="/logistics" exact component={ Logistics } />
+              <Route path="/unifrontendweb" exact component={ Login } />
+              <Route path="/unifrontendweb/dashboard" exact component={ dashboard } />
+              <Route path="/unifrontendweb/logistics" exact component={ Logistics } />
               <Route path="/transfer" exact component={ Transfer } />
               <Route path="/transactions" exact component={ Transaction } />
               <Route path="/acknowledge" exact component={ Acknowledge } />
--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -43,7 +43,7 @@ export default class dashboard extends Component {
                 <div className='dash'> 
                     <div className='widgets'>
                         <Widget 
-                            redirect='/logistics' 
+                            redirect='/unifrontendweb/logistics' 
                             title='S & L' 
                             link='Supply and Logistics'
                             backgroundColor= '#d25f20'
